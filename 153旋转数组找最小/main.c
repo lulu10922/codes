5774159cc276c53dd94501f5e01c029151d4ecc2
lulu10922 <1092298897@qq.com>
2015-09-07 16:53:10 +0800

    见description
    这个版本是有重复时的情况，当mid和left，right的元素都相等时就不知道它属于哪一边了，所以又加了一个按序的函数mininorder，悲剧的是代码的运行时间击败了0.0%的人。甚至直接用顺序查找也能ac。。。

---
+++ 153旋转数组找最小/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findMin(int* nums, int numsSize)
+{
+    if(nums[0]<nums[numsSize-1])
+       return nums[0];
+    int left=0,right=numsSize-1;
+    while(left+1<right)
+    {
+        int mid=(left+right)/2;
+        if(nums[mid]>nums[left])
+            left=mid;
+        else if(nums[mid]<nums[right])
+            right=mid;
+    }
+
+    return nums[right];
+}
+
+int main()
+{
+    int a[]={1,2,4,5,6,7};
+    printf("%d\n",findMin(a,6));
+    return 0;
+}
