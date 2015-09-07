7d062c3e63901b8326ec0ea2a0baa86f854f35ac
lulu10922 <1092298897@qq.com>
2015-09-07 20:42:32 +0800

    更简洁
    跳过前边重复的和后边重复的

--- 154旋转数组找最小02/main.c
+++ 154旋转数组找最小02/main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int mininorder(int *nums,int left,int right)
-{
-    int tmp=nums[left];
-    int i;
-    for(i=left+1;i<=right;++i)
-    {
-        if(nums[i]<tmp)
-            tmp=nums[i];
-    }
-    return tmp;
-}
-
 int findMin(int* nums, int numsSize)
 {
     if(nums[0]<nums[numsSize-1])
@@ -20,14 +8,12 @@ int findMin(int* nums, int numsSize)
     int left=0,right=numsSize-1;
     while(left+1<right)
     {
+        while(left<right&&nums[left]==nums[left+1]) left++;
+        while(left<right&&nums[right]==nums[right-1]) right--;
         int mid=(left+right)/2;
-        //当中间值和左右同时相等时，只能用遍历的方法。
-        if(nums[mid]==nums[left]&&nums[mid]==nums[right])
-            return mininorder(nums,left,right);
-
-        if(nums[mid]>=nums[left])
+        if(nums[mid]>nums[left])
             left=mid;
-        else if(nums[mid]<=nums[right])
+        else if(nums[mid]<nums[right])
             right=mid;
     }
 
